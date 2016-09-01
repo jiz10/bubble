006ee8f36f5290d17317e65797bb48b4d215c67f
Sebastian Garcia Ojeda <jsgarciaojeda@gmail.com>
2016-09-01 14:03:19 -0300

    dynamic msg list

--- src/main/webapp/assets/js/msgAjax.js
+++ src/main/webapp/assets/js/msgAjax.js
@@ -1,5 +1,38 @@
 $(document).ready(function(){
 	
-	alert("fuck YEAH!");
+	
+	$(document).ready(function(){
+		alert("hello");
+		fillMsgList();
+	});
+	
+	function fillMsgList(){
+		var url = "mensajes/";
+		$.ajax({
+			type:'GET',
+			headers: {Accept: 'application/json'},
+			url: url,
+			data: {},
+			success: function(data) {
+				var pasteHTML = "";
+				$.each(data, function(i, val){
+					
+					pasteHTML += "<li class='card text-xs-center tweet radio'>";
+					pasteHTML += "<div class='card-header'>";
+					pasteHTML += "<h4 class='card-title'>@JSGO</h4>";	
+					pasteHTML += "</div>";
+					pasteHTML += "<div class='card-block'>";
+					pasteHTML += "<p class='card-text'>hola</p>";	
+					pasteHTML += "</div>";
+					pasteHTML += "<div class='card-footer text-muted'>2 days ago</div>";
+					pasteHTML += "</li>";
+				
+				});
+				$("#tablonMsg").append(pasteHTML);
+				alert("help");
+			}
+		});
+	}
 	
 });
+
